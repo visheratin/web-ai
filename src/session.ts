ba2b9dacfe59985a950723b9c0835205c7a1888c
Alexander Visheratin <alexvish91@gmail.com>
2022-12-11 00:28:24 -0500

    Added compressed model files.

--- src/session.ts
+++ src/session.ts
@@ -1,4 +1,5 @@
 import * as ort from "onnxruntime-web";
+import * as pako from "pako";
 
 ort.env.wasm.numThreads = 3;
 ort.env.wasm.simd = true;
@@ -6,7 +7,11 @@ ort.env.wasm.proxy = true;
 ort.env.wasm.wasmPaths = "https://edge-ai-models.s3.us-east-2.amazonaws.com/onnx-13/";
 
 export const createSession = async (modelPath: string): Promise<ort.InferenceSession> => {
-  const model_data = await fetch(modelPath).then((resp) => resp.arrayBuffer());
+  let model_data = await fetch(modelPath).then((resp) => resp.arrayBuffer());
+  const extension = modelPath.split(".").pop();
+  if (extension === "gz") {
+    model_data = pako.inflate(model_data);
+  }
   const session = await ort.InferenceSession.create(model_data, {
     executionProviders: ["wasm"],
     graphOptimizationLevel: "all",
--- src/text/models.ts
+++ src/text/models.ts
@@ -26,8 +26,8 @@ export const models: TextMetadata[] = [
     type: TextModelType.Seq2Seq,
     sizeMB: 32,
     modelPaths: new Map<string, string>([
-      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder-quant.onnx"],
-      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init-quant.onnx"],
+      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder-quant.onnx.gz"],
+      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init-quant.onnx.gz"],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
     tags: ["grammar", "t5"],
@@ -39,8 +39,8 @@ export const models: TextMetadata[] = [
     type: TextModelType.Seq2Seq,
     sizeMB: 55,
     modelPaths: new Map<string, string>([
-      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-encoder-quant.onnx"],
-      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder-init-quant.onnx"],
+      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-encoder-quant.onnx.gz"],
+      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder-init-quant.onnx.gz"],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
     tags: ["grammar", "t5"],
@@ -52,8 +52,8 @@ export const models: TextMetadata[] = [
     type: TextModelType.Seq2Seq,
     sizeMB: 122,
     modelPaths: new Map<string, string>([
-      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder.onnx"],
-      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init.onnx"],
+      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-encoder.onnx.gz"],
+      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tiny-decoder-init.onnx.gz"],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
     tags: ["grammar", "t5"],
@@ -65,8 +65,8 @@ export const models: TextMetadata[] = [
     type: TextModelType.Seq2Seq,
     sizeMB: 214,
     modelPaths: new Map<string, string>([
-      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-encoder.onnx"],
-      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder-init.onnx"],
+      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-encoder.onnx.gz"],
+      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/mini-decoder-init.onnx.gz"],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
     tags: ["grammar", "t5"],
@@ -80,11 +80,11 @@ export const models: TextMetadata[] = [
     modelPaths: new Map<string, string>([
       [
         "encoder",
-        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-encoder-quant.onnx",
+        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-encoder-quant.onnx.gz",
       ],
       [
         "decoder",
-        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-decoder-quant.onnx",
+        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-decoder-quant.onnx.gz",
       ],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
@@ -99,11 +99,11 @@ export const models: TextMetadata[] = [
     modelPaths: new Map<string, string>([
       [
         "encoder",
-        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-encoder.onnx",
+        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-encoder.onnx.gz",
       ],
       [
         "decoder",
-        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-decoder.onnx",
+        "https://edge-ai-models.s3.us-east-2.amazonaws.com/feature-extraction/t5-efficient-mini-decoder.onnx.gz",
       ],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
@@ -116,8 +116,8 @@ export const models: TextMetadata[] = [
     type: TextModelType.Seq2Seq,
     sizeMB: 355,
     modelPaths: new Map<string, string>([
-      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/flan/flan-t5-small-encoder.onnx"],
-      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/flan/flan-t5-small-decoder.onnx"],
+      ["encoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/flan/flan-t5-small-encoder.onnx.gz"],
+      ["decoder", "https://edge-ai-models.s3.us-east-2.amazonaws.com/flan/flan-t5-small-decoder.onnx.gz"],
     ]),
     tokenizerPath: "https://edge-ai-models.s3.us-east-2.amazonaws.com/grammar/tokenizer.json",
     tags: ["general", "t5-flan"],
