3ff8401cd57905f51d242308090b30cbd97f4afc
Alexander Visheratin <alexvish91@gmail.com>
2023-03-06 00:42:37 -0500

    Added tokenizers init.

--- src/text/tokenizer.ts
+++ src/text/tokenizer.ts
@@ -1,6 +1,7 @@
-import { WasmTokenizer } from "@visheratin/tokenizers";
+import init, { WasmTokenizer } from "@visheratin/tokenizers";
 
 export const loadTokenizer = async (tokenizerPath: string): Promise<WasmTokenizer> => {
+  await init();
   const response = await fetch(tokenizerPath);
   const tokenizerData = await response.json();
   tokenizerData["padding"] = null;
