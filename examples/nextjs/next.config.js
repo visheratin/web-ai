d43a8106d57a13f1365ceb36e1883a77cf4cbd01
Alexander Visheratin <alexvish91@gmail.com>
2023-03-12 12:56:23 -0400

    Add headers.

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -16,6 +16,24 @@ const nextConfig = {
     };
     return config;
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
