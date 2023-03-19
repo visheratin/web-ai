858b24871ad37f35ce68c10063379e3a8a9eebe8
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2023-03-19 22:25:11 +0100

    Create a tensor when token_type_ids is expected

--- src/text/encoder.ts
+++ src/text/encoder.ts
@@ -21,6 +21,16 @@ export class Encoder implements Model {
       input_ids: input,
       attention_mask: attentionTensor,
     };
+    if (this.session.ortSession) {
+      const inputNames = await this.session.inputNames();
+      if (inputNames.includes("token_type_ids")) {
+        const typeIdsTensor = new ort.Tensor("int64", new BigInt64Array(input.data.length).fill(0n), [
+          1,
+          input.data.length,
+        ]);
+        encoderFeeds["token_type_ids"] = typeIdsTensor;
+      }
+    }
     const output = await this.session.run(encoderFeeds);
     const result = output[this.outputName];
     return result;
