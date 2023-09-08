a0dfb4714b0a0a18887d2ad545caf1f7afed700c
Alexander Visheratin <alexvish91@gmail.com>
2023-09-08 19:44:43 -0400

    Latest changes.

--- src/node/sessionParams.ts
+++ src/node/sessionParams.ts
@@ -2,7 +2,7 @@ import { SessionParameters } from "../common.js";
 
 export const SessionParams: SessionParameters = {
   numThreads: 0,
-  executionProviders: ["wasm"],
+  executionProviders: ["cpu"],
   memoryLimitMB: 0,
   cacheSizeMB: 2500,
   wasmRoot: "https://web-ai-models.org/onnx-15/",
